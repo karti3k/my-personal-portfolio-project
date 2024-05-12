c9d988c17b30dbe8b4ad8275a9ff151ef1bcfaa1
Kartik <kartikbhatia2304@gmail.com>
2024-05-12 23:31:12 +0530

    animation & font part is left, navbar almost completed!

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,20 +1,35 @@
 import navIcon1 from '../Images/nav-icon1.svg';
 import navIcon2 from '../Images/nav-icon2.svg';
 import navIcon3 from '../Images/nav-icon3.svg';
+import logo from '../Images/logo.svg';
 import './NavBar.css'
 
 const NavBar = ()=>{
     return (
         <div>
             <nav className="top-nav">
+                <section className='sec1'>
+                <span className='nav-logo'>
+                    <img src={logo}></img>
+                </span>
+                <ul className='link-to-sections'>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Skills</a></li>
+                    <li><a href='#'>Projects</a></li>
+                </ul>
+                </section>
+                <section className='sec2'>
+                <ul className='social-icons'>
+                    <li><a href='#'><img src={navIcon1}></img></a></li>
+                    <li><a href='#'><img src={navIcon2}></img></a></li>
+                    <li><a href='#'><img src={navIcon3}></img></a></li>
+                </ul>
+                <span className='top-but-box'>
                 <button className='connect-but'>
                     Let's connect
                 </button>
-                <ul className='social-icons'>
-                    <li><a><img src={navIcon1}></img></a></li>
-                    <li><a><img src={navIcon2}></img></a></li>
-                    <li><a><img src={navIcon3}></img></a></li>
-                </ul>
+                </span>
+                </section>
             </nav>
         </div>
     )
