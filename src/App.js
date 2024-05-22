ae15d4845822600ad5fe7bbbd335a81679ffe3a4
Kartik <kartikbhatia2304@gmail.com>
2024-05-22 22:10:57 +0530

    Added SkillsSec.js and its css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './Components/NavBar';
 import Banner from './Components/Banner';
+import SkillsSec from './Components/SkillsSec';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <SkillsSec />
     </div>
   );
 }
---
+++ src/Components/SkillsSec.js
@@ -0,0 +1,24 @@
+import './SkillsSec.css';
+
+const SkillsSec = () =>{
+    return (
+        <section className='skills-sec'>
+            <div className='skills-div'>
+                <h2>Skills</h2>
+                <h3>HTML5</h3>
+                <h3>CSS3</h3>
+                <h3>JavaScript</h3>
+                <h3>React JS</h3>
+                <h3>Java</h3>
+                <h3>Python</h3>
+                <h3>Canva</h3>
+                <h3>Figma</h3>
+                <h3>Power Director Pro</h3>
+                <h3>VS Code</h3>
+                <h3>Git & Github</h3>
+                <h3>Basic Linux Commands</h3>
+            </div>
+        </section>
+    );
+}
+export default SkillsSec;
