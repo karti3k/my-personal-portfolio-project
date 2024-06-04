a3b841925cc95e61769f14d03eba104aaa5814ba
Kartik <kartikbhatia2304@gmail.com>
2024-06-04 20:41:07 +0530

    updated thumbnails of creation section!

--- src/Components/ProjectSec.js
+++ src/Components/ProjectSec.js
@@ -1,33 +1,33 @@
 import React, { useState } from 'react';
 import './ProjectSec.css';
-import projImg6 from '../Images/projImg6.jpg';
-import projImg8 from '../Images/projImg8.jpg';
-import projImg4 from '../Images/projImg4.jpg';
-import designimg1 from '../Images/designimg1.jpg';
-import designimg2 from '../Images/designimg2.jpg';
-import designimg3 from '../Images/designimg3.jpg';
-import contImg4 from '../Images/contImg4.jpg';
-import contImg6 from '../Images/contImg6.jpg';
-import contImg8 from '../Images/contImg8.jpg';
+import newProjectImg1 from '../Images/newProjectImg1.jpg';
+import newProjectImg2 from '../Images/newProjectImg2.jpg';
+import newProjectImg3 from '../Images/newProjectImg3.jpg';
+import newDesignImg1 from '../Images/newDesignImg1.jpg';
+import newDesignImg2 from '../Images/newDesignImg2.jpg';
+import newDesignImg3 from '../Images/newDesignImg3.jpg';
+import newContentImg1 from '../Images/newContentImg1.jpg';
+import newContentImg2 from '../Images/newContentImg2.jpg';
+import newContentImg3 from '../Images/newContentImg3.jpg';
 const ProjectSec = () => {
     const [selectedCategory, setSelectedCategory] = useState('tech');
     const [previousCategory, setPreviousCategory] = useState(null);
 
     const projects = {
         tech: [
-            { img: projImg4, link: 'https://github.com/karti3k/Employee-Activity-Dashboard/blob/main/README.md', text: 'GitHub' },
-            { img: projImg6, link: 'https://github.com/karti3k/MovieFinder/blob/main/README.md', text: 'GitHub' },
-            { img: projImg8, link: 'https://github.com/acmditu/ACM-Website', text: 'GitHub' },
+            { img: newProjectImg1, link: 'https://github.com/karti3k/Employee-Activity-Dashboard/blob/main/README.md', text: 'GitHub' },
+            { img: newProjectImg2, link: 'https://github.com/karti3k/MovieFinder/blob/main/README.md', text: 'GitHub' },
+            { img: newProjectImg3, link: 'https://github.com/acmditu/ACM-Website', text: 'GitHub' },
         ],
         designs: [
-            { img: designimg1, link: 'https://www.behance.net/gallery/184804873/Jainism-Website-Design-Idea', text: 'Behance' },
-            { img: designimg2, link: 'https://www.behance.net/gallery/180831449/Expense-Manger-Web-App-UI-Design-Idea-', text: 'Behance' },
-            { img: designimg3, link: 'https://www.behance.net/gallery/179476323/Some-of-my-poster-designs-and-logo-designs', text: 'Behance' },
+            { img: newDesignImg1, link: 'https://www.behance.net/gallery/184804873/Jainism-Website-Design-Idea', text: 'Behance' },
+            { img: newDesignImg2, link: 'https://www.behance.net/gallery/180831449/Expense-Manger-Web-App-UI-Design-Idea-', text: 'Behance' },
+            { img: newDesignImg3, link: 'https://www.behance.net/gallery/179476323/Some-of-my-poster-designs-and-logo-designs', text: 'Behance' },
         ],
         content: [
-            { img: contImg4, link: 'https://youtu.be/ZSl-AeR_eMo?si=4FsmqZoU26f_-ZmX', text: 'YouTube' },
-            { img: contImg6, link: 'https://youtu.be/OKYskIJ3Z54?si=k7DlnA_NneE2lTdM', text: 'YouTube' },
-            { img: contImg8, link: 'https://youtu.be/qe1jQE7SrjQ?si=0P8AAl_n8pveh9J4', text: 'YouTube' },
+            { img: newContentImg1, link: 'https://youtu.be/ZSl-AeR_eMo?si=4FsmqZoU26f_-ZmX', text: 'YouTube' },
+            { img: newContentImg2, link: 'https://youtu.be/OKYskIJ3Z54?si=k7DlnA_NneE2lTdM', text: 'YouTube' },
+            { img: newContentImg3, link: 'https://youtu.be/qe1jQE7SrjQ?si=0P8AAl_n8pveh9J4', text: 'YouTube' },
         ],
     };
 
